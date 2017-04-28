b117cd332fdb6676d51811193ef776d90a941408
Meltem <melltemkilic@gmail.com>
2017-04-28 01:21:21 +0000

    post / end point added for the resource url submission. Doesn't have the entire functionality yet

--- routes/resource.js
+++ routes/resource.js
@@ -5,7 +5,9 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  // localhost:8080/api/resource/
   router.get("/", (req, res) => {
+    // data helper will come here
     knex
       .select("*")
       .from("users")
@@ -13,6 +15,14 @@ module.exports = (knex) => {
         res.json(results);
     });
   });
+
+  router.post("/", (req, res) => {
+    if(err) {
+      console.error('post is not successful');
+      throw err;
+    }
+    console.log('resource url posted successfully');
+  })
 
   return router;
 }
