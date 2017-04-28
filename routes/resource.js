e4ad24231a0b36984cfbb7a79d3e03c545410cac
Meltem <melltemkilic@gmail.com>
2017-04-28 21:10:47 +0000

    getscreenshot function added under the post / route

--- routes/resource.js
+++ routes/resource.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../lib/util/data-helpers.js');
+const getScreenShot = require('../lib/util/getscreenshot');
 
 module.exports = () => {
 
@@ -44,6 +45,23 @@ module.exports = () => {
     //   ,'/path_of_stuff/asdfasdf.jpg')
     // .then((results) => {});
   });
+
+  router.post("/", (req, res) => {
+    if(err) {
+      console.error('post is not successful');
+      throw err;
+    }
+    const url = 'http://github.com';
+    const viewport = '&viewport=1440x900';
+    const fullpage = '&fullpage=1';
+
+    getScreenShot.getScreenShot(url, viewport, fullpage, (path) => {
+      console.log('path: ', path);
+      return path;
+    });
+    //data helper function should come here and save the image to the database
+    console.log('resource url posted successfully');
+  })
 
   return router;
 }
