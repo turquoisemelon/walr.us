c0c833eae2e7ac513ead89913ea5479497a25a95
Meltem <melltemkilic@gmail.com>
2017-04-28 22:47:06 +0000

    post / route is in progress

--- routes/resource.js
+++ routes/resource.js
@@ -15,18 +15,29 @@ module.exports = (knex) => {
   });
 
   router.post("/", (req, res) => {
-    if(err) {
-      console.error('post is not successful');
-      throw err;
+    if (!req.body.text) {
+      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      return;
     }
     const url = 'http://github.com';
     const viewport = '&viewport=1440x900';
     const fullpage = '&fullpage=1';
 
-    getScreenShot.getScreenShot(url, viewport, fullpage, (path) => {
-      console.log('path: ', path);
-      return path;
-    });
+    // const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    // const tweet = {
+    //   user: user,
+    //   content: {
+    //     text: req.body.text
+    //   },
+    //   created_at: Date.now()
+    // };
+
+    // getScreenShot.getScreenShot(url, viewport, fullpage, (path) => {
+    //   console.log('path: ', path);
+    //   return path;
+    // });
+    console.log(req.body);
+
     //data helper function should come here and save the image to the database
     console.log('resource url posted successfully');
   })
