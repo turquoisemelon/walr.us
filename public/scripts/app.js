cc6a85d0e14f950c91f76bff61f8b06849b73acc
Andrew J Royce <andrewjroyce@gmail.com>
2017-04-29 18:30:29 +0000

    small dh spelling app.js wip functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -41,8 +41,6 @@ $(document).ready(function() {
   });
 
   };
-
-
   (function(d, s, id){
    var js, fjs = d.getElementsByTagName(s)[0];
    if (d.getElementById(id)) {return;}
@@ -51,23 +49,78 @@ $(document).ready(function() {
    fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // function createTweetElement(tweet) {
-  //   let $tweet =
-  //     `<article class="tweet-container">
-  //               <header class="user"><img src="${tweet.user.avatars.small}">
-  //                   <h2 class="name"> ${tweet.user.name}</h2>
-  //                   <span class="handle"> ${tweet.user.handle} </span>
-  //               </header>
-  //               <section class="content">
-  //                   <p class="message" >${tweet.content.text}</p>
-  //               </section>
-  //               <span class="tweet-footer">
-  //                   <span class="created_at">${timeSince(tweet.created_at)}</span>
-  //                   <i class="fa fa-flag" aria-hidden="true"></i>
-  //                   <i class="fa fa-retweet" aria-hidden="true"></i>
-  //                   <i class="fa fa-heart" aria-hidden="true"></i>
-  //               </span>
-  //           </article>`
-  //   return $tweet;
-})
+
+  function createPostElement(post) {
+    let $post =     
+    <article>
+    <div class="col s4 asset">
+        <article class="post-container">
+            <header class="title">
+                <p class="name"> ${fb.user.name}</p> <!-- http:/graph.facebook.com/USERID/picture/ -->
+                <span class="tag"> ${post.tags} </span>
+            </header>
+            <section class="content">
+                <div class="image">
+                    <img src="http://i.ebayimg.com/images/g/IUUAAOSwv2JXwNU2/s-l500.jpg" alt="" />
+                    <h3 class="truncate"><span>Title of Resource:<span class='spacer'></span><br/><span class='spacer'></span>Description</span> 
+                    </h3>
+                </div>
+            </section>
+
+            <span class="post-footer">
+    <div class="fb-comments" data-href="http://walr.us/1" data-width="320px" data-numposts="5"></div>
+            <span class="c-rating" id="counter0"></span>
+            <div class="fb-like" data-href="http://walr.us/1" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
+            </span>
+        </article>
+return $post;
+};
+
+function renderPost(posts) {
+    var result = "";
+    posts.forEach(function(post) {
+      result = createPostsElement(post) + result
+    })
+    $('.post-area').html(result)
+  };
+
+  function loadPosts() {
+    $.ajax({
+      url: 'http://localhost:8080/post',
+      method: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(data) {
+        renderPosts(data);
+      }
+    })
+  };
+  loadPosts();
+
+  $("#postarea").on("submit", function(event) {
+    event.preventDefault();
+    if ($("textarea").val() == "" || null) {
+      alert('Say Something')
+    }
+    if ($("textarea").val().length > 140) {
+      alert('Stahp')
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/posts',
+        data: $('textarea').serialize(),
+        success: function() {
+          $('textarea').val('');
+          $('span.counter').html('140');
+          loadPosts();
+        }
+      });
+    }
+  });
+};
+
+
+
+}
 
